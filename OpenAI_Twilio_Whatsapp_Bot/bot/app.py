9a032b3ac9d14fa53f9f6ad960afb4e31f60e15e
jayeshbhawsar <jayesh.bhawsar@vkaps.com>
2023-02-07 19:26:30 +0530

    First commit

---
+++ OpenAI_Twilio_Whatsapp_Bot/bot/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, session
+from twilio.twiml.messaging_response import MessagingResponse
+from bot import ask, append_interaction_to_chat_log
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv("OPENAI_API_KEY")
+
+@app.route('/bot', methods=['POST'])
+def bot():
+    incoming_msg = request.values['Body']
+    chat_log = session.get('chat_log')
+    answer = ask(incoming_msg, chat_log)
+    session['chat_log'] = append_interaction_to_chat_log(incoming_msg, answer,
+                                                         chat_log)
+    msg = MessagingResponse()
+    msg.message(answer)
+    return str(msg)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ OpenAI_Twilio_Whatsapp_Bot/bot/bot.py
@@ -0,0 +1,36 @@
+from dotenv import load_dotenv
+from random import choice
+from flask import Flask, request
+import os
+import openai
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+completion = openai.Completion()
+
+start_sequence = "\nAI:"
+restart_sequence = "\nHuman: "
+session_prompt = " YOUR CHEF'S STORY HERE "
+
+
+def ask(question, chat_log=None):
+    prompt_text = prompt_text = f'{chat_log}{restart_sequence}: {question}{start_sequence}:'
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt_text,
+        temperature=0.9,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.6,
+        stop=[" Human:"]
+    )
+    
+    story = response['choices'][0]['text']
+    return str(story)
+
+
+def append_interaction_to_chat_log(question, answer, chat_log=None):
+    if chat_log is None:
+        chat_log = session_prompt
+    return f'{chat_log}{restart_sequence} {question}{start_sequence}{answer}'
